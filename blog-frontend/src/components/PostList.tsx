9bc4a103f5114eededd51f6d92c7951ed301c05d
dotm <desde.el.mar96@gmail.com>
2024-07-19 20:26:11 +0700

    use infinite scroll library

--- blog-frontend/src/components/PostList.tsx
+++ blog-frontend/src/components/PostList.tsx
@@ -2,11 +2,12 @@ import { useLazyGetPostListQuery } from "@/redux/apiSlice";
 import { PostData } from "@/utils/types";
 import { useRouter } from 'next/router';
 import { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default function PostList() {
   const router = useRouter()
   const [lastOldestPostId, setLastOldestPostId] = useState<number | undefined>(undefined)
-  const [pageLimit, setPageLimit] = useState(2)
+  const [pageLimit, setPageLimit] = useState(10)
   const [reachedEndOfPage, setReachedEndOfPage] = useState(false)
   const [trigger, result] = useLazyGetPostListQuery();
   const [postList, setPostList] = useState<PostData[]>([])
@@ -29,11 +30,11 @@ export default function PostList() {
 
   //trigger initial load
   //commented out in development because this cause duplicate fetch when hot-reloading
-  // useEffect(() => {
-  //   if (postList.length === 0 && !reachedEndOfPage) {
-  //     fetchPost()
-  //   }
-  // }, [postList, reachedEndOfPage]);
+  useEffect(() => {
+    if (postList.length === 0 && !reachedEndOfPage) {
+      fetchPost()
+    }
+  }, [postList, reachedEndOfPage]);
 
   return (
     <div className="space-y-3">
@@ -65,58 +66,40 @@ export default function PostList() {
           !result.isLoading && postList.length > 0
           ?
           <ul role="list" className="pt-3 grid grid-cols-1 gap-6">
-            {postList
-              .map(post => {
-                return (
-                  <li
-                    key={post.id}
-                    className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-gray-900 hover:bg-black text-center shadow border"
-                  >
-                    <div className="flex flex-1 flex-col p-8">
-                      <h3 className="text-sm font-medium text-gray-200">{post.title}</h3>
-                      <p className="text-sm text-gray-200">{post.content}</p>
-                    </div>
-                  </li>
-                )
-              })
-            }
+            <InfiniteScroll
+              dataLength={postList.length} //This is important field to render the next data
+              next={fetchPost}
+              hasMore={!reachedEndOfPage}
+              loader={
+                <p style={{ textAlign: 'center' }}>
+                  Loading...
+                </p>
+              }
+              endMessage={
+                <p style={{ textAlign: 'center' }}>
+                  No more posts to load
+                </p>
+              }
+            >
+              {
+                postList.map(post => {
+                  return (
+                    <li
+                      key={post.id}
+                      className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-gray-900 hover:bg-black text-center shadow border"
+                    >
+                      <div className="flex flex-1 flex-col p-8">
+                        <h3 className="text-sm font-medium text-gray-200">{post.title}</h3>
+                        <p className="text-sm text-gray-200">{post.content}</p>
+                      </div>
+                    </li>
+                  )
+                })
+              }
+            </InfiniteScroll>
           </ul>
           :
           <></>
-        }
-        {
-          reachedEndOfPage
-          ?
-          <div className="flex flex-1 justify-between sm:justify-end mt-2 pt-2 gap-2 items-center">
-            <p>
-              No more posts to load
-            </p>
-          </div>
-          :
-          <div className="flex flex-1 justify-between sm:justify-end mt-2 pt-2 gap-2 items-center">
-            <p>
-              Limit per page:
-            </p>
-            <input
-              id="pageLimit"
-              name="pageLimit"
-              type="number"
-              value={pageLimit}
-              onChange={(event) => {
-                setPageLimit(parseInt(event.target.value) ?? 0)
-              }}
-              className="block w-[60px] text-center rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-            <button
-              id="get-next-page"
-              onClick={() => {
-                fetchPost()
-              }}
-              className="relative ml-3 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0"
-            >
-              {postList.length === 0 && !reachedEndOfPage ? "Get First Page" : "Get Next Page"}
-            </button>
-          </div>
         }
       </div>
     </div>
